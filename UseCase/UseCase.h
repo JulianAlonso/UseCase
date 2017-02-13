72f1a6dfb7b2c1df637062ba0b15a8633715a03e
Julián Alonso <julian.alonso.dev@gmail.com>
2017-02-13 16:12:56 +0100

    Initial commit

---
+++ UseCase/UseCase.h
@@ -0,0 +1,19 @@
+//
+//  UseCase.h
+//  UseCase
+//
+//  Created by Julián Alonso Carballo on 13/2/17.
+//  Copyright © 2017 com.julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UseCase.
+FOUNDATION_EXPORT double UseCaseVersionNumber;
+
+//! Project version string for UseCase.
+FOUNDATION_EXPORT const unsigned char UseCaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UseCase/PublicHeader.h>
+
+
